dc5bfb964f89687805d42839845aa286f7a3e629
Ivan <i85chiosa@gmail.com>
2022-03-29 23:06:33 +0200

    Übung Array

--- mar-03-29-22/übung.js
+++ mar-03-29-22/übung.js
@@ -84,3 +84,94 @@ console.table(euroCities);
 // Create another variable named asianCities and assign an array of at least 5 cities to the variable.
 const asianCities = ["India", "China", "Bangladesh", "South Korea", "Turkey"];
 console.log(asianCities);
+
+
+
+// Capitalize. Create a program that capitalizes the first letter of each element in an array of names. Examples:
+// ["matt", "sara", "lara"] ➞ ["Matt", "Sara", "Lara"]
+// ["samuel", "MARIA", "luke", "mary"] ➞ ["Samuel", "Maria", "Luke", "Mary"]
+// ["Cynthia", "Karen", "Jane", "Carrie"] ➞ ["Cynthia", "Karen", "Jane", "Carrie"]
+
+function capitalize(arr) {
+    for (  let i = 0; i <arr.length; i++){
+       let first = arr[i] [0].toUpperCase();
+       let rest = arr[i].substring(1).toLowerCase();
+       arr[i] = first + rest;
+        // arr[i] = arr[i] [0];
+        // names[i] = names[i][0].toUpperCase() + names[i].substring(1).toLowerCase();
+    }
+    return console.table(arr);  // result 
+    // ┌─────────┬────────┐
+    // │ (index) │ Values │
+    // ├─────────┼────────┤
+    // │    0    │ 'Matt' │
+    // │    1    │ 'Sara' │
+    // │    2    │ 'Lara' │
+    // └─────────┴────────┘
+    // [nodemon] clean exit
+}
+const arr1 = ["matt", "sara", "lara"];
+const arr2 = []
+capitalize(arr1);
+
+ console.log("--------");
+// Repeat it. Create a program with two variables: "item" and "times". Create a program that repeats the "item" as many times as specified by "times". The first variable ("item") is the item that needs repeating while the second argument ("times") is the number of times the item is to be repeated. Print the result in an array. Examples:
+// ("example", 3) ➞ ["example", "example", "example"]
+function repeat(str,num){
+    let arr = [];
+    for (let i = 0; i < num; i++){
+        arr[i] = str;
+    }
+    return console.table(arr);
+}
+repeat("Ivan", 2);
+
+
+
+// Word Ranking. Create a function that takes a string of words and returns the highest scoring word.
+// wordRank("The quick brown fox.") ➞ "brown"
+// wordRank("Nancy is very pretty.") ➞ "pretty"
+// wordRank("Check back tomorrow, man!") ➞ "tomorrow"
+// wordRank("Today is Wednesday.") ➞ "Wednesday"
+function wordRank(str){
+    let longesWord = "";
+    let strToArr = str.split(" ");
+    // console.log(strToArr);
+
+    for ( let i = 0; i < strToArr.length; i++){
+        if (longesWord.length < strToArr[i].length) longesWord = strToArr[i];
+    }
+    return console.log(longesWord);;
+}
+wordRank("Ivan ich cool something");
+
+
+
+// The Greater Numbers. Create a function which accepts two arguments: the first argument being an array of numbers, and the second argument being a number. The function should return the elements of the array which are greater than the second argument.
+// i.e.
+// findGreatest([3, 4, 5], 4) ➞ 
+// findGreatest([10, 20, 30], 12) ➞ 20, 30
+// findGreatest([0, 10, 3], 4) ➞ 10
+
+
+function findGreatest (arr, num){
+    let str = "";
+    for ( let i = 0; i <arr.length; i++){
+        if (arr[i] > num) {
+            str += `${arr[i]}`;
+        }
+    }
+    return console.log(str);
+}
+
+findGreatest([1,2,3,5], 8);
+
+
+// . Dictionary. Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
+// Notes:
+// If none of the words match, return an empty array.
+// Keep the filtered array in the same relative order as the original array of words.
+// Examples:
+// dictionary("bu", ["button", "breakfast", "border"]) ➞ ["button"]
+// dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]) ➞ ["triplet", "tries", trip"]
+// dictionary("beau", ["pastry", "delicious", "name", "boring"]) ➞ []
---
+++ mar-03-30-22/index.js
@@ -0,0 +1,23 @@
+// The Greater Numbers. Create a function which accepts two arguments: the first argument being an array of numbers, and the second argument being a number. The function should return the elements of the array which are greater than the second argument.
+// i.e.
+// findGreatest([3, 4, 5], 4) ➞ 
+// findGreatest([10, 20, 30], 12) ➞ 20, 30
+// findGreatest([0, 10, 3], 4) ➞ 10
+
+function findGreatest (arr,num) {
+    let str = "";
+
+
+    console.log(str);
+}
+
+// function findGreatest (arr, num){
+//     let str = "";
+//     for ( let i = 0; i <arr.length; i++){
+//         if (arr[i] > num) {
+//             str += `${arr[i]}`;
+//         }
+//     }
+//     return console.log(str);
+// }
+// findGreatest([1,2,3,5], 8);
