6c854f0a59109945eb1cef1840778deab115318b
Ivan <i85chiosa@gmail.com>
2022-04-01 21:27:23 +0200

    Übung function

--- april/01-04-22/index.js
+++ april/01-04-22/index.js
@@ -92,3 +92,19 @@ let sum12 = 20;
 
 
 
+
+
+
+// Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox '
+function convertUpp (str){
+    let conv = str.toLowerCase().split(" ");
+
+    for (let i = 0; i < conv.length; i++) {
+        conv[i] = conv[i][0].toUpperCase() + conv[i].substring(1);
+    }
+    conv = conv.join(" ")
+    return console.log(conv);
+}
+convertUpp("the quick brown fox");
