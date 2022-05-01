a172b359603241eb926c7ee18e759217f6be3f50
Ivan <i85chiosa@gmail.com>
2022-05-01 17:30:56 +0200

    Übung

--- april/27-04-22/übung.js
+++ april/27-04-22/übung.js
@@ -18,3 +18,73 @@ const alphabetic = alpha.sort();
 console.log(alphabetic);
 
 
+// for in 
+const personObj = {
+    firstName: "Chiosa",
+    lastName: "Ivan",
+    age: 36
+}
+for ( const prop in personObj) {
+    console.log(`${prop} :${personObj[prop]}`);
+};
+
+// for of
+const arrName = ["mario", "Ivan", "Ecaterina"];
+for ( const name of arrName) {
+     console.log(name);
+};
+
+
+// do while
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i <= 10);
+
+// while 
+let index = 0;
+while (index <= 6) {
+    console.log(index);
+    index ++;
+}
+// for each
+const cities = ["Moldavia", "Parigi", "Köln", "Berlin"];
+cities.forEach((ele) => console.log(ele));
+
+console.log("------");
+for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+}
+
+// find
+const num = [1,2,3,4,4,5,5,] ;
+const numberFind = num.find((ele) => ele == 5);
+console.log(numberFind);
+
+// 
+
+const numbers = [1,1,1,1,2,2,];
+const findNumResult = numbers.find((ele) => ele == 2);
+const filterNumResult = numbers.filter((ele) => ele == 2);
+console.log("----find----");
+console.log(findNumResult);
+console.log("----filter----");
+console.log(filterNumResult);
+
+// findIndex
+const numFindIndex = numbers.findIndex((ele) => ele == 2);
+console.log(numFindIndex);
+
+// findIndexOf
+console.log("--- indexOf----");
+const numFindIndexOf = numbers.indexOf(2);
+console.log(numFindIndexOf);
+
+console.log("--- some----");
+const num1 = [5,6,6,6,2,22,32,1];
+console.log(num1.some((ele) => ele == 6));
+
+console.log("--- every----");
+const numArr1 = [6,6,6];
+console.log(numArr1.every((ele) => ele == 6));
