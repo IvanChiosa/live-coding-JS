1cdba64f2ec53618441ca633cb6fa24332b8bfff
Ivan <i85chiosa@gmail.com>
2022-05-10 14:21:39 +0200

    Übungen

--- mai/09-05-22/übungen.js
+++ mai/09-05-22/übungen.js
@@ -7,7 +7,7 @@
 // })();
 
 // Imediatly Invockt Function Expression
-const sum = ((x,y) => x + y) (5,6);
+const sum = ((x, y) => x + y)(5, 6);
 console.log(sum);
 
 
@@ -15,7 +15,7 @@ const userCarData = {
    year: 2022,
    model: "Seat",
    speed: 260,
-   get fullCarName(){
+   get fullCarName() {
       return `This is di model years ${this.year}, und die model ist ${this.model} il spped is ${this.speed}`;
    },
    set fullCarName(str) {
@@ -36,7 +36,7 @@ userCarData.fullCarName = "1999 BMW 100";
 console.log(userCarData.fullCarName);
 
 
-function Person (name, age, add) {
+function Person(name, age, add) {
    this.name = name;
    this.age = age;
    this.add = add;
@@ -44,3 +44,84 @@ function Person (name, age, add) {
 
 const ivanData = new Person("Ivan", 37, "Koln");
 console.log(ivanData);
+
+
+console.log("-------Calculator------");
+// Miserable Parody of a Calculator
+// Create a function that will handle simple math expressions. The input is an expression in the form of a string.
+// Examples
+// calculator("23+4") ➞ 27
+// calculator("45-15") ➞ 30
+// calculator("13+2-5*2") ➞ 5
+// calculator("49/7*2-3") ➞ 11
+
+const calculator = (str) => {
+   return eval(str);
+};
+console.log(calculator("23+4"));
+console.log(calculator("45-15"));
+console.log(calculator("13+2-5*2"));
+console.log(calculator("49/7*2-3"));
+
+
+// Visa oer Master, write a function that checks if the user has entered a valid card data
+// Example:
+// 412178679036597 -> this is Visa card, I need your money
+// 512178679036597 -> this is Master card, I need your money
+// 34121786790365 -> this is AmEpx card, I need your money
+// 11111 -> this is not correct
+// de343ee -> this is not correct
+
+const cardValidation = (num) => {
+   let cardVisa = num.length;
+   let cardMaster = num.length;
+   let cardAmExp = num.length;
+
+   if (cardVisa === 16) {
+      return `This is Visa card, I need your money!`;
+   }
+   if (cardMaster == 15) {
+      return `This is Master card, I need your money!`
+   }
+   if (cardAmExp === 14) {
+      return `This is AmEpx card, I need your money!`
+   }
+   if (isNaN(num)) {
+      return `You have to write only a numbers`
+   }
+   return `This is not correct`
+};
+console.log(cardValidation("1234567890123456"));
+console.log(cardValidation("123456789012345"));
+console.log(cardValidation("12345678901234"));
+console.log(cardValidation("de343ee"));
+console.log(cardValidation("11111"));
+
+
+// Phone number, Write a function that checks if a user give a correct German (landline Berlin 030) phone number
+// 030-1247893 -> I'll call now
+// 333-332222 -> this is not a phone number
+// 030-33 -> This is not complete !
+
+const phoneNumber = (num) => {
+   let correctNumber = num.length;
+   let landlineBerlin = num.substring(0,3);
+
+   if (correctNumber == 10 ) {
+      return `I'll call now`;
+   }
+   if (landlineBerlin != "030") {
+      return `This is not a phone number von Berlin`;
+   }
+   if (correctNumber < 10) {
+      return `This is not complete!`
+   }
+};
+console.log(phoneNumber("0301247893"));
+console.log(phoneNumber("333332222"));
+console.log(phoneNumber("03033"));
+
+
+
+
+
