8ffbdf077988eae7c9d915177e56796dbac2c3a9
Ivan <i85chiosa@gmail.com>
2022-05-18 14:46:19 +0200

    Übungen

--- mai/18-05-22/index.js
+++ mai/18-05-22/index.js
@@ -3,9 +3,40 @@
 const axios = require("axios");
 // const axios = require("./cool"); // referenz zu deine cool.js colling
 
+
+// users
+// axios
+//  .get("https://jsonplaceholder.typicode.com/users") // methode zu seine was und wocher du muss gehen zu info zu haben // Protocol adresse e rooter
+//  .then((res) => res.data.map(({name,address,phone}) =>  // schau die losung in terminal
+//    console.log(
+//        `This person ${name} lives in ${address.street} you can all them on ${phone}`
+//        )
+//     )
+//   ) 
+//  .catch((err) => console.log(err)) // fur die error 
+// ;
+// console.log("Ivan cool");
+
+
+// todo
+// axios
+//  .get("https://jsonplaceholder.typicode.com/todo/10") // methode zu seine was und wocher du muss gehen zu info zu haben // Protocol adresse e rooter
+//  .then((res) => res.data.map(({name,address,phone}) =>  // schau die losung in terminal
+//    console.log(
+//        `This person ${name} lives in ${address.street} you can all them on ${phone}`
+//        )
+//     )
+//   ) 
+//  .catch((err) => console.log(err)) // fur die error 
+// ;
+// console.log("Ivan cool");
+
+
+
+// comments
 axios
- .get("https://jsonplaceholder.typicode.com/users")
- .then((res) => res.data.map(({name,address,phone}) => 
+ .get("https://jsonplaceholder.typicode.com/coments") // methode zu seine was und wocher du muss gehen zu info zu haben // Protocol adresse e rooter
+ .then((res) => res.data.map(({name,address,phone}) =>  // schau die losung in terminal
    console.log(
        `This person ${name} lives in ${address.street} you can all them on ${phone}`
        )
